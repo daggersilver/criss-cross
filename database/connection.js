437c5ceb2d8316948f24a3a3e1ff62f8f90ace07
daggersilver <shitcodder@gmail.com>
2022-04-17 18:04:28 +0530

    commit 2

--- database/connection.js
+++ database/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/crisscross');
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.s6okb.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`);
 
 mongoose.connection.on("connected", () => {
     console.log("database connected");
